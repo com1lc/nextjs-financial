e27482897383d7803e8a2769c4f83de310921293
com1lc <plato2h@gmail.com>
2023-12-18 22:32:20 +0700

    analytics

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
